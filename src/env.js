b8b2b30437bb7951f760a195532c770c4dccdacc
Taeho Kim <dittos@gmail.com>
2016-06-25 22:59:19 +0900

    Add location methods to environment

--- src/env.js
+++ src/env.js
@@ -10,14 +10,29 @@ export interface Environment {
   setTitle(title: string): void;
   getPath(): string;
   getQuery(): WireObject;
+  getLocationKey(): string;
   getPreloadData(): ?WireObject;
+  setLocationChangeListener(listener: () => void): void;
+  replaceLocation(key: string): void;
+  pushLocation(path: string, key: string): void;
 }
+
+// TODO: detect pushState support
 
 export class DefaultEnvironment {
   container: Node;
+  locationChangeListener: ?() => void;
 
   constructor(container: Node) {
     this.container = container;
+    this.locationChangeListener = null;
+    window.addEventListener('popstate', event => {
+      // TODO: normalize difference between browsers
+      // https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onpopstate
+      if (this.locationChangeListener) {
+        this.locationChangeListener();
+      }
+    }, false);
   }
 
   render(element: React.Element) {
@@ -36,7 +51,24 @@ export class DefaultEnvironment {
     return querystring.parse(location.search.substring(1));
   }
 
+  getLocationKey() {
+    return history.state && history.state.key;
+  }
+
   getPreloadData() {
     return window.preloadData;
+  }
+
+  setLocationChangeListener(listener: () => void) {
+    this.locationChangeListener = listener;
+  }
+
+  replaceLocation(key: string) {
+    const path = location.pathname + location.search;
+    history.replaceState({ key }, '', path);
+  }
+
+  pushLocation(path: string, key: string) {
+    history.pushState({ key }, '', path);
   }
 }
