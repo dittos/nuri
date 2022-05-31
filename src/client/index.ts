62c05a5c70db2e2de6465935d01f122110146531
Taeho Kim <dittos@gmail.com>
2022-05-31 23:39:04 +0900

    onPreloadDataReady

--- src/client/index.ts
+++ src/client/index.ts
@@ -20,16 +20,22 @@ export function render<L>(app: App<L>, container: Element, loader: L, preloadDat
   return controller;
 }
 
-export function bootstrap<L>(app: App<L>, loader: L, callback: (controller: AppController<L>) => void) {
+export function onPreloadDataReady(callback: (preloadData: any) => void) {
   const globalVariable = (window as any)[globalVariableName];
-  if (!globalVariable) {
+  if (!globalVariable.preloadData) {
     // HTML is not rendered yet
     (window as any)[globalVariableName] = (preloadData: any) => {
       (window as any)[globalVariableName].preloadData = preloadData;
-      bootstrap(app, loader, callback);
+      callback(preloadData);
     };
     return;
   }
-  const controller = render(app, document.getElementById(containerElementId)!, loader, globalVariable.preloadData);
-  callback(controller);
+  callback(globalVariable.preloadData);
+}
+
+export function bootstrap<L>(app: App<L>, loader: L, callback: (controller: AppController<L>) => void) {
+  onPreloadDataReady(preloadData => {
+    const controller = render(app, document.getElementById(containerElementId)!, loader, preloadData);
+    callback(controller);
+  });
 }
